49516c9517dbfa3ebea724de6cf1d1dd8f81b1ce
J-HyeonSu <luvian5028@gmail.com>
2025-06-04 14:11:17 +0900

    feat: add jump, groundcheck

--- Assets/02. Scripts/CatController.cs
+++ Assets/02. Scripts/CatController.cs
@@ -1,21 +1,49 @@
+using System;
 using UnityEngine;
 
 public class CatController : MonoBehaviour
 {
     private Rigidbody2D catRb;
+    private Animator catAnim;
+    
     public float jumpPower = 10f;
+    public bool isGround = false;
     
-    
-    void Awake()
+    public int jumpCount = 0;
+
+    void Start()
     {
         catRb = GetComponent<Rigidbody2D>();
+        catAnim = GetComponent<Animator>();
     }
-    
+
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && jumpCount < 2)
         {
+            catAnim.SetTrigger("Jump");
+            
             catRb.AddForceY(jumpPower, ForceMode2D.Impulse);
+            jumpCount++; // 1씩 증가
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            catAnim.SetBool("isGround", true);
+            jumpCount = 0;
+            isGround = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            catAnim.SetBool("isGround", false);
+            isGround = false;
         }
     }
 }
