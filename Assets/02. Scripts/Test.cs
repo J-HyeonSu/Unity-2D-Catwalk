c89f43ba503693115f4222deb9f7a62eaf1fb037
J-HyeonSu <luvian5028@gmail.com>
2025-06-09 21:23:38 +0900

    feat: Catjump and scene

---
+++ Assets/02. Scripts/Test.cs
@@ -0,0 +1,29 @@
+using System;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    public enum Calcu
+    {
+        plus,
+        minus,
+        multiply,
+        divide
+    };
+
+    public Calcu cc;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch (cc)
+        {
+            
+        }
+    }
+}
